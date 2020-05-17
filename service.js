107e14c8d570923fd8aaffa34981a95fbac281d0
Mikita Zayats <nikitaza0@gmail.com>
2020-05-17 15:43:06 +0300

    fix homework

--- service.js
+++ service.js
@@ -1,13 +1,16 @@
 const csv = require('csvtojson')
-let fs = require('fs');
+const fs = require('fs');
+const readline = require('readline');
 
 
 export function transformAndMove(fromFile, toFile) {
 
+    let writeStream = fs.createWriteStream(toFile);
+
     csv()
         .fromFile(fromFile)
         .on('data', (data) => {
-            fs.appendFileSync(toFile, data);
+            writeStream.write(data)
         })
         .on('error', (error) => {
             console.log(error);
@@ -15,4 +18,15 @@ export function transformAndMove(fromFile, toFile) {
         .on('done', () => {
             console.log("Done");
         });
+}
+
+export function reverseInput(){
+    let rl = readline.createInterface({
+        input: process.stdin
+      });
+      
+      rl.on('line', (line) => {
+        let result = line.split("").reverse().join("");
+        console.log(result);
+      })
 }
